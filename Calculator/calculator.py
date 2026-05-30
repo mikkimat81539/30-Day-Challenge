2685eafd1028b7b4e5bf5b454b2c0c438401eedc
mikkimat81539 <mikkimat81539@gmail.com>
2026-05-30 00:24:53 +0000

    created screen

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,5 +1,15 @@
 # MAKE A CALCULATOR IN PYTHON GUI
+import PyQt6, sys
 
-numInput = input("Enter numbers: ")
+from PyQt6.QtWidgets import QMainWindow, QApplication 
 
-print(eval(numInput))
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle("Calculator")
+
+app = QApplication(sys.argv)
+screen = MainWindow()
+screen.resize(450, 500)
+screen.show()
+app.exec()
