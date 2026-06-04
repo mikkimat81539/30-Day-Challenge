ed328515b1fc669d5af1f5870cd75cd483193208
mikkimat81539 <mikkimat81539@gmail.com>
2026-06-04 21:55:28 +0000

    added Label

--- Alarm/main.py
+++ Alarm/main.py
@@ -1,6 +1,8 @@
 # CREATE AN ALARM THAT IS A GUI USING PYQT
 
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 
 import sys
 
@@ -9,13 +11,43 @@ import sys
 
 #sys.argv contains at least one element (the script name)
 
-app = QApplication(["Alarm"])
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle("Alarm")
+		self.setFixedSize(QSize(500, 600))
+		self.setStyleSheet("background-color: blue;") # Color format similar to css
+
+	def Textbox(self):
+		# Here we will input our time
+		pass
+
+	def Labeling(self):
+		# Here we will put any necessary text (Ex: the colon (:), title)
+		title = QLabel("Hello")	
+
+		font = title.font() # initialize font
+		font.setPointSize(30) # Set the size
+		title.setFont(font) # Setting the font
+		title.setStyleSheet("color: white;") # Font color		
+
+		# Set Label Widget
+		self.setCentralWidget(title)
+
+	def Buttons(self):
+		# Here will be the buttons to save alarm
+		pass
+
+app = QApplication([])
 
 
 # Create a Qt widget, which will be our window.
-window = QWidget()
+screen = MainWindow()
 
-window.show() # IMPORTANT!!!!! Windows are hidden by default so you have to show it.
+# DRAW WIDGETS
+screen.Labeling()
+
+screen.show() # IMPORTANT!!!!! Windows are hidden by default so you have to show it.
 
 # Start the event loop.
 app.exec()
