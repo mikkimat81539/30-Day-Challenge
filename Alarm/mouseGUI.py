e22873f7a7c0aef19c9540420ba1c6958b746dc3
mikkimat81539 <mikkimat81539@gmail.com>
2026-06-06 23:09:27 +0000

    displayed time on screen

--- Alarm/mouseGUI.py
+++ Alarm/mouseGUI.py
@@ -9,21 +9,40 @@ class MainWindow(QMainWindow):
 		self.setWindowTitle("Mouse Test")
 		self.setFixedSize(500, 300)
 
+		self.layout = QVBoxLayout()
+
 	def Button(self):
+		# Draw Button
 		button = QPushButton("Pressed Me")
 		button.setFixedSize(100, 50)
 		button.setCursor(Qt.CursorShape.PointingHandCursor)
 
-		# Check if button was click
 		# button.setCheckable(True) # makes a button behave like a toggle switch instead of a normal push button.
 
 		# DATETIME
 		Hour = dt.datetime.now().hour
 		Minutes =  dt.datetime.now().minute
 
-		button.clicked.connect(lambda: print(f"{Hour}:{Minutes}"))
+		time_format = dt.time(hour=Hour, minute=Minutes)
+
+		# Text
+		Text = QLabel(f"{time_format}")
+		Text.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+		# Text.hide() # hide text
+		Text.setVisible(False)
+
+		
+		# Check if buttton is clicked
+		button.clicked.connect(lambda: Text.setVisible(True))
 	
-		self.setCentralWidget(button)
+		# Store Widgets
+		self.layout.addWidget(button)
+		self.layout.addWidget(Text)
+
+		widget = QWidget()
+		widget.setLayout(self.layout)
+
+		self.setCentralWidget(widget)
 
 # INIT APPLICATION
 app = QApplication([])
