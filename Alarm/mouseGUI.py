3548bb9a0ac034cd83f2ad6ffee9bd6c712dd94a
mikkimat81539 <mikkimat81539@gmail.com>
2026-06-06 22:42:09 +0000

    added functionality to button

--- Alarm/mouseGUI.py
+++ Alarm/mouseGUI.py
@@ -1,8 +1,6 @@
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import Qt
 
-from pynput import mouse
-
 class MainWindow(QMainWindow):
 	def __init__(self):
 		super().__init__()
@@ -13,6 +11,10 @@ class MainWindow(QMainWindow):
 		button = QPushButton("Pressed Me")
 		button.setFixedSize(100, 50)
 		button.setCursor(Qt.CursorShape.PointingHandCursor)
+
+		# Check if button was click
+		button.setCheckable(True)
+		button.clicked.connect(lambda: print("Pressed"))
 	
 		self.setCentralWidget(button)
 
