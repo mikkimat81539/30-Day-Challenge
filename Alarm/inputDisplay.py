cb3e06ad0cd4f699e9141885d1c93e2707f04d84
mikkimat81539 <mikkimat81539@gmail.com>
2026-06-07 00:45:35 +0100

    added textbox

--- Alarm/inputDisplay.py
+++ Alarm/inputDisplay.py
@@ -1,5 +1,6 @@
 # GOAL IS TO DISPLAY WHAT IS INPUTTED WHEN BUTTON IS PRESSED
 
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import *
 
 class MainWindow(QMainWindow):
@@ -9,9 +10,32 @@ class MainWindow(QMainWindow):
 		self.setFixedSize(500, 300)
 		self.setStyleSheet("background-color: #9c8649;")
 
+		self.layout = QVBoxLayout()
+
+	def Text_Field(self):
+		inputField = QLineEdit()
+		inputField.setMaxLength(10)
+		inputField.setStyleSheet(""" 
+			QLineEdit{
+				border: 3px solid black;
+			}
+		""")
+
+		inputField.setContentsMargins(0, 0, 0, 0)
+		self.layout.addWidget(inputField, alignment=Qt.AlignmentFlag.AlignTop)
+		
+		widget = QWidget()
+		widget.setLayout(self.layout)
+		self.setCentralWidget(widget)
+
+	def Button(self):
+		pass
+
 app = QApplication([])
 
 screen = MainWindow()
+
+screen.Text_Field()
 
 screen.show()
 
