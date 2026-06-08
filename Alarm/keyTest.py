fb944ffa7e7cfcad878c472269d23930a9cd400b
mikkimat81539 <mikkimat81539@gmail.com>
2026-06-08 18:33:09 +0100

    added button with pointer cursor

--- Alarm/keyTest.py
+++ Alarm/keyTest.py
@@ -1,5 +1,6 @@
-from PyQt6.QtGui import QKeyEvent
-from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QKeyEvent, QCursor
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton
 
 class MainWindow(QMainWindow):
 	def __init__(self):
@@ -8,6 +9,23 @@ class MainWindow(QMainWindow):
 		self.setFixedSize(500, 300)
 		self.setStyleSheet("background-color: #914ead;")
 
+	def Button(self):
+		# Draw Button
+		button = QPushButton("Press Me")
+		button.setFixedSize(100, 50)
+	
+#		button.setStyleSheet("""
+#			QPushButton {
+#				border: 3px solid black;
+#			}
+#		""")	
+
+		button.setCursor(Qt.CursorShape.PointingHandCursor)
+
+		self.setCentralWidget(button)
+
+		
+
 # init application
 app = QApplication([])
 
@@ -15,6 +33,7 @@ app = QApplication([])
 screen = MainWindow()
 
 # Draw methods
+screen.Button()
 
 # Show screen
 screen.show()
