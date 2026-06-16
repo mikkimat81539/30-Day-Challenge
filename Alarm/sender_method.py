89ffb5831d90c1fb9929ab102b8f13a0b0be9c25
mikkimat81539 <mikkimat81539@gmail.com>
2026-06-16 18:44:01 +0100

    changed background color of screen

--- Alarm/sender_method.py
+++ Alarm/sender_method.py
@@ -4,7 +4,7 @@
 # When BTN 2 is clicked display -> POOP
 # When BTN 3 is clicked resize to 500, 200
 
-from PyQt6.QtWidgets import QWidget, QMainWindow, QPushButton, QApplication
+from PyQt6.QtWidgets import QWidget, QMainWindow, QPushButton, QApplication, QHBoxLayout
 
 
 class MainWindow(QMainWindow):
@@ -12,14 +12,26 @@ class MainWindow(QMainWindow):
 		super().__init__()
 		self.setWindowTitle("Sender Method Test")
 		self.setFixedSize(500, 550)
+		self.setStyleSheet("background-color: #8fd5d9;")
 		# print(self.width()) # This allows me to grab the width of my widget
 		# print(self.height()) # This allows me to grab the height of my widget
 
+	def Button(self):
+		unoBtn = QPushButton("UNO")
+		poopBtn = QPushButton("POOP")
+		resizeBtn = QPushButton("Resize Me")
 
+		buttonList = [unoBtn, poopBtn, resizeBtn]
+
+		for i in buttonList:
+			pass
 
 app = QApplication([])
 
 screen = MainWindow()
+
+# Draw Widgets
+screen.Button()
 
 # Show screen
 screen.show()
