101aaffa718dcccd640088212e7d3e5b24e25bac
mikkimat81539 <mikkimat81539@gmail.com>
2026-06-29 21:09:35 +0100

    created button and number text increments by 1

--- Alarm/delete_test.py
+++ Alarm/delete_test.py
@@ -4,26 +4,52 @@
 # I will store each number in a sorteed list (database)
 
 from PyQt6.QtWidgets import *
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QCursor
+
 
 class MainWindow(QMainWindow):
 	def __init__(self):
 		super().__init__()
+		
+		# set screen attributes
 		self.setWindowTitle("Delete Test")
 		self.setFixedSize(500, 300)
 		self.setStyleSheet("background-color: pink")
 
+		# Layout Container
+		self.container = QWidget()
+		self.layout = QGridLayout(self.container)
+		self.setCentralWidget(self.container)
+
+		# Button Counter
+		self.counter = 1
+
 		self.database = sorted([]) # This is for storing the numbers
-		print(self.database)
 
 
-	def Increment_Buttons(self):
+	def Label(self):
 		pass
 
+	def Increment_Button(self):
+		self.button = QPushButton(str(self.counter))
+		self.button.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+		self.button.setFixedSize(100, 50)
+
+		self.layout.addWidget(self.button)
+
+		self.button.clicked.connect(self.button_clicked)
+
+	def button_clicked(self):
+		self.counter += 1
+		self.button.setText(str(self.counter))
 
 
 app = QApplication([])
 
 screen = MainWindow()
+
+screen.Increment_Button()
 
 screen.show()
 
